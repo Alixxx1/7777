19e9367869d645b7b38a60c0c48f039f5080fe68
Alixxx1 <59136354+Alixxx1@users.noreply.github.com>
2019-12-24 01:21:59 +0200

    Create 7777.py

---
+++ 7777.py
@@ -0,0 +1,24 @@
+import os 
+os.system ("clear")
+
+print (" 😈😈😈😈😈😈😈😈😈😈 ")
+print (" 😎                😎 ")
+print (" 😎                😎 ")
+print (" 😎                😎 ")
+print (" 😎                😎 ")
+print (" 😎                😎 ") 
+print (" 😎                😎 ")
+print (" 😈😈😈😈😈😈😈😈😈😈 ")
+ 
+ 
+ 
+ 
+ali = input ("Type is pssyurd *: ")
+
+if  ali == "ali1212 " :
+    print ("Helo")
+  
+
+ 
+    
+ 
